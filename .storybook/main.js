7f89b4054dfaff1c15f1368e126ec7da5b1d7554
Shawn Jones <sjones5@stubhub.com>
2022-08-26 14:54:39 -0700

    resolve package issues

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,15 @@
-const path = require('path');
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/preset-create-react-app',
-    "@storybook/addon-a11y"
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
   ],
-};
+  "framework": "@storybook/react",
+  transform: {
+    '\\.[jt]sx?$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    "^react($|/.+)": "<rootDir>/node_modules/react$1", // makes sure all React imports are running off of the one in this package.
+  },
+}
